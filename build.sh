bc1275a32ccee6796e153305ca5585680213bf14
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-11-01 11:55:34 +0100

    Do not produce an error message if "build" directory is already there.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
 
-mkdir build
+mkdir -p build
 cd build
 # generate solution for GCC
 cmake ..

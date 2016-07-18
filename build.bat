2b36f52b6e5c22e96407817fa7e840dd7085d6d2
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2016-07-18 18:13:30 +0200

    Add an explicit CMake error message on missing googletest submodule
     - build.bat now exit on error instead of trying to build or run tests

--- build.bat
+++ build.bat
@@ -7,11 +7,19 @@ cd build
 
 @REM Generate a Visual Studio solution for latest version found
 cmake -DSQLITECPP_BUILD_EXAMPLES=ON -DSQLITECPP_BUILD_TESTS=ON ..
+if ERRORLEVEL 1 goto onError
 
 @REM Build default configuration (ie 'Debug')
 cmake --build .
+if ERRORLEVEL 1 goto onError
 
 @REM Build and run tests
 ctest --output-on-failure
+if ERRORLEVEL 1 goto onError
 
+cd ..
+exit
+
+:onError
+@echo An error occured!
 cd ..
--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
+#!/bin/sh
 # Copyright (c) 2012-2016 Sébastien Rombauts (sebastien.rombauts@gmail.com)
 #
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
+
+# exit on firts error
+set -e
+
 mkdir -p build
 cd build
 
 # Generate a Makefile for GCC (or Clang, depanding on CC/CXX envvar)
 cmake -DCMAKE_BUILD_TYPE=Debug -DSQLITECPP_USE_GCOV=OFF -DSQLITECPP_BUILD_EXAMPLES=ON -DSQLITECPP_BUILD_TESTS=ON ..
+if [ $? != 0 ] ; then exit
 
 # Build (ie 'make')
 cmake --build .

da725ed1cfe4b5e91b2abc5d59830287753dde37
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2016-07-26 08:42:12 +0200

    Fix compiler warning (unused variable)

--- src/Database.cpp
+++ src/Database.cpp
@@ -28,7 +28,6 @@ const int   OPEN_READONLY   = SQLITE_OPEN_READONLY;
 const int   OPEN_READWRITE  = SQLITE_OPEN_READWRITE;
 const int   OPEN_CREATE     = SQLITE_OPEN_CREATE;
 const int   OPEN_URI        = SQLITE_OPEN_URI;
-const int   OPEN_MEMORY     = SQLITE_OPEN_MEMORY;
 
 const int   OK              = SQLITE_OK;
 

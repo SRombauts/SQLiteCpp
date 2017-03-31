4c339031f062c73abe0081207e4ff0cfa0e9e17c
Timothy Rae <perceptualchaos2@gmail.com>
2017-03-31 12:12:46 +0900

    Remove const from Database::isUnencrypted() return type Compiling with gcc pedantic mode was leading to a warning: "type qualifiers ignored on function return type"

--- src/Database.cpp
+++ src/Database.cpp
@@ -265,7 +265,7 @@ void Database::rekey(const std::string& aNewKey) const
 }
 
 // Test if a file contains an unencrypted database.
-const bool Database::isUnencrypted(const std::string& aFilename)
+bool Database::isUnencrypted(const std::string& aFilename)
 {
     if (aFilename.length() > 0) {
         std::ifstream fileBuffer(aFilename.c_str(), std::ios::in | std::ios::binary);

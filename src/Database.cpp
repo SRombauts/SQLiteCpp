eadd166d75c93afe354fef1227de82a029dd9397
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2020-01-12 22:05:53 +0100

    Fix a warning testing return of strncmp

--- src/Database.cpp
+++ src/Database.cpp
@@ -310,7 +310,7 @@ Header Database::getHeaderInfo(const std::string& aFilename)
 
     // If the "magic string" can't be found then header is invalid, corrupt or unreadable
     strncpy(pHeaderStr, pBuf, 16);
-    if (!strncmp(pHeaderStr, "SQLite format 3", 15) == 0)
+    if (strncmp(pHeaderStr, "SQLite format 3", 15) != 0)
     {
         throw SQLite::Exception("Invalid or encrypted SQLite header in file " + aFilename);
     }

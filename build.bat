a0f72c779717cf4908aab962ffa4f43f4d5b3e31
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2020-01-12 22:11:38 +0100

    Hide logs of goto in windows build.bat script

--- build.bat
+++ build.bat
@@ -7,15 +7,15 @@ cd build
 
 @REM Generate a Visual Studio solution for latest version found
 cmake -DSQLITECPP_BUILD_EXAMPLES=ON -DSQLITECPP_BUILD_TESTS=ON ..
-if ERRORLEVEL 1 goto onError
+@if ERRORLEVEL 1 goto onError
 
 @REM Build default configuration (ie 'Debug')
 cmake --build .
-if ERRORLEVEL 1 goto onError
+@if ERRORLEVEL 1 goto onError
 
 @REM Build and run tests
 ctest --output-on-failure
-if ERRORLEVEL 1 goto onError
+@if ERRORLEVEL 1 goto onError
 
 goto onSuccess
 

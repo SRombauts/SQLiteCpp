e872fcdb08e7e48b50bf618f9402e3eaa4f4dd48
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2024-08-21 22:15:24 +0200

    cpplint: fail the build on errors

--- cpplint.py
+++ cpplint.py
@@ -4807,8 +4807,8 @@ def main():
     ProcessFile(filename, _cpplint_state.verbose_level)
   _cpplint_state.PrintErrorCounts()
 
-  # SRombauts: do not break build for cpplint style warnings
-  #sys.exit(_cpplint_state.error_count > 0)
+  # SRombauts: break the build for cpplint style warnings
+  sys.exit(_cpplint_state.error_count > 0)
 
 
 if __name__ == '__main__':
